8ae082ea97fa51f70db86d3a8295193bc88ffa5d
Trevor Vaughn Hauck <hauck.trevor@gmail.com>
2023-05-27 12:49:51 -0400

    add raspi cmd

--- go-server/main.go
+++ go-server/main.go
@@ -25,7 +25,7 @@ func executeCmd(message string, sleep bool) {
 func main() {
 	path, _ := os.Getwd()
 	ffmpegCmd := "ffmpeg -fflags +genpts -i tcp://192.168.0.%v:2222 -f rtsp -c copy rtsp://localhost:8554/%v"
-	go executeCmd(fmt.Sprintf(ffmpegCmd, "50", "cam"), true)
-	go executeCmd(fmt.Sprintf(ffmpegCmd, "184", "cam-2"), true)
+	go executeCmd(fmt.Sprintf(ffmpegCmd, "184", "cam"), true)
+	go executeCmd(fmt.Sprintf(ffmpegCmd, "50", "cam-2"), true)
 	executeCmd(fmt.Sprintf("%s/mediamtx", path), false)
 }
