eddd1dbd510a4caf25b82ece22bc71d0d0dd0880
Trevor Vaughn Hauck <hauck.trevor@gmail.com>
2023-05-31 13:11:01 -0400

    Update WHEP client, add prettier

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,111 +1,56 @@
-import { useCallback, useEffect, useState } from "react";
-import ReactPlayer from "react-player/lazy";
-import useWebSocket, { ReadyState } from "react-use-websocket";
+import { useEffect, useState } from "react";
+import WHEPClient from "../utils/WHEPClient";
+
+// const stream1Url = "http://192.168.0.140:8889/cam-1";
+// const stream2Url = "http://192.168.0.140:8889/cam-2";
 
 export default function Home() {
     const [show, setShow] = useState(false);
-    const stream1Url = "http://192.168.0.140:8889/cam";
-    const stream2Url = "http://192.168.0.140:8889/cam-2";
+    new WHEPClient("1");
+    // new WHEPClient("2");
 
     useEffect(() => {
-        // const canPlay = ReactPlayer.canPlay(stream1Url);
-        // console.log(canPlay);
         setTimeout(() => {
             setShow(true);
-        }, 500);
+        }, 200);
     });
-    //
+
     if (!show) {
         return null;
     }
 
-    const vidHeight = screen.height / 2.5;
-    const vidWidth = screen.width - 150;
+    // const screenRatioHW = screen.height / screen.width;
+
+    // const vidHeight = screenRatioHW < 1.0 ? screen.height / 2.5 + 90 : screen.height / 2 - 250;
+    // const vidWidth = screenRatioHW < 1.0 ? screen.width - 150 : screen.width - 20;
 
     return (
         <div>
-            <h1>Videos</h1>
-            <div className="flex flex-row items-center ">
-                <iframe height={vidHeight} width={vidWidth} src={stream2Url} ></iframe>
-                <iframe height={vidHeight} width={vidWidth} src={stream1Url} ></iframe>
+            <div className={`video-container`}>
+                <video
+                    width={"100%"}
+                    autoPlay
+                    id="video-1"
+                    muted
+                ></video>
+                <video
+                    width={"100%"}
+                    autoPlay
+                    id="video-2"
+                    muted
+                ></video>
             </div>
         </div>
     );
 }
 
-export const WebSocketDemo = () => {
-    //Public API that will echo messages sent to it back to the client
-    // const [socketUrl, setSocketUrl] = useState("ws://localhost:8083/ws/");
-    const [socketUrl, setSocketUrl] = useState("ws://localhost:8083/ws");
-
-    const [messageHistory, setMessageHistory] = useState<any[]>([]);
-
-    const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
-
-    useEffect(() => {
-        if (lastMessage !== null) {
-            setMessageHistory((prev) => {
-                prev.push(lastMessage);
-
-                return [...prev];
-            });
-        }
-    }, [lastMessage, setMessageHistory]);
-
-    const handleClickChangeSocketUrl = useCallback(
-        () => setSocketUrl("wss://demos.kaazing.com/echo"),
-        []
-    );
-
-    const handleClickSendMessage = useCallback(() => sendMessage("Hello"), []);
-
-    const connectionStatus = {
-        [ReadyState.CONNECTING]: "Connecting",
-        [ReadyState.OPEN]: "Open",
-        [ReadyState.CLOSING]: "Closing",
-        [ReadyState.CLOSED]: "Closed",
-        [ReadyState.UNINSTANTIATED]: "Uninstantiated",
-    }[readyState];
-
-    const handleGetStream = async () => {
-        try {
-            const response = await fetch("http://localhost:8083/stream");
-            console.log(response);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    return (
-        <div>
-            <button onClick={handleGetStream}>get stream</button>
-            <br />
-            <br />
-
-            <button onClick={handleClickChangeSocketUrl}>
-                Click Me to change Socket Url
-            </button>
-            <br />
-            <br />
-
-            <button
-                onClick={handleClickSendMessage}
-                disabled={readyState !== ReadyState.OPEN}
-            >
-                Click Me to send {'Hello'}
-            </button>
-            <br />
-            <br />
-
-            <span>The WebSocket is currently {connectionStatus}</span>
-            {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
-            <br />
-            <br />
-
-            <ul>
-                {messageHistory.map((message, idx) => (
-                    <span key={idx}>{message ? message.data : null}</span>
-                ))}
-            </ul>
-        </div>
-    );
-};
+// <iframe
+//                     width="100%"
+//                     height="500px"
+//                     src={stream2Url}
+//                 />
+//                 <iframe
+//                     width="100%"
+//                     height="500px"
+//                     src={stream1Url}
+//                 />
---
+++ client/utils/WHEPClient.ts
@@ -0,0 +1,236 @@
+const streamBase = "http://192.168.0.140:8889";
+const restartPause = 2000;
+
+const linkToIceServers = (links: any) =>
+    links !== null
+        ? links.split(", ").map((link: string) => {
+              const m = link.match(
+                  /^<(.+?)>; rel="ice-server"(; username="(.*?)"; credential="(.*?)"; credential-type="password")?/i,
+              );
+              const ret = {
+                  urls: m ? [m[1]] : [],
+                  username: "",
+                  credential: "",
+                  credentialType: "",
+              };
+
+              if (m && m[3] !== undefined) {
+                  ret.username = m[3];
+                  ret.credential = m[4];
+                  ret.credentialType = "password";
+              }
+
+              return ret;
+          })
+        : [];
+
+const parseOffer = (offer: any) => {
+    const ret: { iceUfrag: string; icePwd: string; medias: string[] } = {
+        iceUfrag: "",
+        icePwd: "",
+        medias: [],
+    };
+
+    for (const line of offer.split("\r\n")) {
+        if (line.startsWith("m=")) {
+            ret.medias.push(line.slice("m=".length));
+        } else if (ret.iceUfrag === "" && line.startsWith("a=ice-ufrag:")) {
+            ret.iceUfrag = line.slice("a=ice-ufrag:".length);
+        } else if (ret.icePwd === "" && line.startsWith("a=ice-pwd:")) {
+            ret.icePwd = line.slice("a=ice-pwd:".length);
+        }
+    }
+
+    return ret;
+};
+
+const generateSdpFragment = (offerData: any, candidates: any) => {
+    const candidatesByMedia: any = {};
+    for (const candidate of candidates) {
+        const mid = candidate.sdpMLineIndex;
+        if (candidatesByMedia[mid] === undefined) {
+            candidatesByMedia[mid] = [];
+        }
+        candidatesByMedia[mid].push(candidate);
+    }
+
+    let frag =
+        "a=ice-ufrag:" + offerData.iceUfrag + "\r\n" + "a=ice-pwd:" + offerData.icePwd + "\r\n";
+
+    let mid = 0;
+
+    for (const media of offerData.medias) {
+        if (candidatesByMedia[mid] !== undefined) {
+            frag += "m=" + media + "\r\n" + "a=mid:" + mid + "\r\n";
+
+            for (const candidate of candidatesByMedia[mid]) {
+                frag += "a=" + candidate.candidate + "\r\n";
+            }
+        }
+        mid++;
+    }
+
+    return frag;
+};
+
+export default class WHEPClient {
+    fetchUrl: string;
+    stream: string;
+    pc: null | RTCPeerConnection = null;
+    restartTimeout: null | number;
+    eTag: null | string;
+    queuedCandidates: any[];
+    offerData: any;
+
+    constructor(stream: string) {
+        this.pc = null;
+        this.restartTimeout = null;
+        this.eTag = "";
+        this.queuedCandidates = [];
+        this.stream = stream;
+        this.fetchUrl = `${streamBase}/cam-${stream}/whep`;
+
+        this.start();
+    }
+
+    start() {
+        console.log("requesting ICE servers");
+
+        fetch(this.fetchUrl, {
+            method: "OPTIONS",
+        })
+            .then((res) => this.onIceServers(res))
+            .catch((err) => {
+                console.log("error: " + err);
+                this.scheduleRestart();
+            });
+    }
+
+    onIceServers(res: any) {
+        this.pc = new RTCPeerConnection({
+            iceServers: linkToIceServers(res.headers.get("Link")),
+        });
+
+        const direction = "sendrecv";
+        this.pc.addTransceiver("video", { direction });
+        this.pc.addTransceiver("audio", { direction });
+
+        this.pc.onicecandidate = (evt) => this.onLocalCandidate(evt);
+        this.pc.oniceconnectionstatechange = () => this.onConnectionState();
+
+        this.pc.ontrack = (evt) => {
+            console.log("new track:", evt.track.kind);
+            const videoEl: HTMLVideoElement | null = document.getElementById(
+                `video-${this.stream}`,
+            ) as HTMLVideoElement;
+            if (videoEl) {
+                videoEl.srcObject = evt.streams[0];
+            }
+        };
+
+        this.pc.createOffer().then((desc) => {
+            this.offerData = parseOffer(desc.sdp);
+            this.pc?.setLocalDescription(desc);
+
+            console.log("sending offer");
+            fetch(this.fetchUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/sdp",
+                },
+                body: JSON.stringify(desc),
+            })
+                .then((res) => {
+                    if (res.status !== 201) {
+                        throw new Error("bad status code");
+                    }
+                    this.eTag = res.headers.get("E-Tag");
+                    return res.json();
+                })
+                .then((answer) => this.onRemoteDescription(answer))
+                .catch((err) => {
+                    console.log("error: " + err);
+                    this.scheduleRestart();
+                });
+        });
+    }
+
+    onConnectionState() {
+        if (this.restartTimeout !== null) {
+            return;
+        }
+
+        console.log("peer connection state:", this.pc?.iceConnectionState);
+
+        switch (this.pc?.iceConnectionState) {
+            case "disconnected":
+                this.scheduleRestart();
+        }
+    }
+
+    onRemoteDescription(answer: any) {
+        if (this.restartTimeout !== null) {
+            return;
+        }
+
+        this.pc?.setRemoteDescription(new RTCSessionDescription(answer));
+
+        if (this.queuedCandidates.length !== 0) {
+            this.sendLocalCandidates(this.queuedCandidates);
+            this.queuedCandidates = [];
+        }
+    }
+
+    onLocalCandidate(evt: any) {
+        if (this.restartTimeout !== null) {
+            return;
+        }
+
+        if (evt.candidate !== null) {
+            if (this.eTag === "") {
+                this.queuedCandidates.push(evt.candidate);
+            } else {
+                this.sendLocalCandidates([evt.candidate]);
+            }
+        }
+    }
+
+    sendLocalCandidates(candidates: any) {
+        fetch(this.fetchUrl, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/trickle-ice-sdpfrag",
+                "If-Match": this.eTag ? this.eTag : "",
+            },
+            body: generateSdpFragment(this.offerData, candidates),
+        })
+            .then((res) => {
+                if (res.status !== 204) {
+                    throw new Error("bad status code");
+                }
+            })
+            .catch((err) => {
+                console.log("error: " + err);
+                this.scheduleRestart();
+            });
+    }
+
+    scheduleRestart() {
+        if (this.restartTimeout !== null) {
+            return;
+        }
+
+        if (this.pc !== null) {
+            this.pc.close();
+            this.pc = null;
+        }
+
+        this.restartTimeout = window.setTimeout(() => {
+            this.restartTimeout = null;
+            this.start();
+        }, restartPause);
+
+        this.eTag = "";
+        this.queuedCandidates = [];
+    }
+}
