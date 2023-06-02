0bd61e7cd36004c23a7fe90c96cf3eb0e2f0743f
Trevor Vaughn Hauck <hauck.trevor@gmail.com>
2023-06-01 21:18:54 -0400

    update go program to output streams to mp4 files. Attempt to signal interrupt and restart every hour

--- go-server/main.go
+++ go-server/main.go
@@ -7,11 +7,7 @@ import (
 	"time"
 )
 
-func executeCmd(message string, sleep bool) int {
-
-	if sleep {
-		time.Sleep(1 * time.Second)
-	}
+func executeCmd(message string) (command *exec.Cmd, err error) {
 
 	cmd := exec.Command("sh", "-c", message)
 
@@ -19,16 +15,34 @@ func executeCmd(message string, sleep bool) int {
 
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error running command: ", err)
-		return 1
+		return nil, err
 	}
-	return 0
+	return cmd, nil
+}
+
+func executeFfmpegCmd(message string) int {
+	time.Sleep(1 * time.Second)
+	for {
+		execRes, err := executeCmd(message)
+		if err != nil {
+			return 1
+		}
+		time.Sleep(5 * time.Second)
+		execRes.Process.Signal(os.Interrupt)
+	}
+
 }
 
 func main() {
+	hr, _, _ := time.Now().Clock()
+	fmt.Println(hr)
+	// executeFfmpegCmd(fmt.Sprintf("echo %v", hr))
+
+	// run the start, but then restart the ffmpegs every hour with a new time?
 	path, _ := os.Getwd()
-	ffmpegCmd := "ffmpeg -fflags +genpts -i tcp://192.168.0.%v:2222 -f rtsp -c copy rtsp://localhost:8554/%v"
-	go executeCmd(fmt.Sprintf(ffmpegCmd, "184", "cam-1"), true)
-	go executeCmd(fmt.Sprintf(ffmpegCmd, "50", "cam-2"), true)
-	// go executeCmd("cd ../client; yarn start", false)
-	executeCmd(fmt.Sprintf("%s/mediamtx", path), false)
+	ffmpegCmd := "ffmpeg -fflags +genpts -i tcp://192.168.0.%v:2222 -f rtsp -c copy rtsp://localhost:8554/%v -f mp4 -c copy ../raw/hour-cam-%v-%v.mp4 -y"
+	go executeFfmpegCmd(fmt.Sprintf(ffmpegCmd, "184", "cam-1", "1", hr))
+	go executeFfmpegCmd(fmt.Sprintf(ffmpegCmd, "50", "cam-2", "2", hr))
+	// go executeCmd("cd ../client; yarn start")
+	executeCmd(fmt.Sprintf("%s/mediamtx", path))
 }
