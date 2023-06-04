69286b2a2c87e7f33d929f40839600dfae394f40
Trevor Vaughn Hauck <hauck.trevor@gmail.com>
2023-06-04 08:51:46 -0400

    Temp disable file saving on ffmpeg script

--- go-server/main.go
+++ go-server/main.go
@@ -40,9 +40,14 @@ func main() {
 
 	// run the start, but then restart the ffmpegs every hour with a new time?
 	path, _ := os.Getwd()
-	ffmpegCmd := "ffmpeg -fflags +genpts -i tcp://192.168.0.%v:2222 -f rtsp -c copy rtsp://localhost:8554/%v -f mp4 -c copy ../raw/hour-cam-%v-%v.mp4 -y"
-	go executeFfmpegCmd(fmt.Sprintf(ffmpegCmd, "184", "cam-1", "1", hr))
-	go executeFfmpegCmd(fmt.Sprintf(ffmpegCmd, "50", "cam-2", "2", hr))
+	// ffmpegCmd := "ffmpeg -fflags +genpts -i tcp://192.168.0.%v:2222 -f rtsp -c copy rtsp://localhost:8554/%v -f mp4 -c copy ../raw/hour-cam-%v-%v.mp4 -y"
+	// go executeFfmpegCmd(fmt.Sprintf(ffmpegCmd, "184", "cam-1", "1", hr))
+	// go executeFfmpegCmd(fmt.Sprintf(ffmpegCmd, "50", "cam-2", "2", hr))
+	// go executeFfmpegCmd(fmt.Sprintf(ffmpegCmd, "112", "cam-3", "3", hr))
+	ffmpegCmd := "ffmpeg -fflags +genpts -i tcp://192.168.0.%v:2222 -f rtsp -c copy rtsp://localhost:8554/%v "
+	go executeFfmpegCmd(fmt.Sprintf(ffmpegCmd, "184", "cam-1"))
+	go executeFfmpegCmd(fmt.Sprintf(ffmpegCmd, "50", "cam-2"))
+	go executeFfmpegCmd(fmt.Sprintf(ffmpegCmd, "112", "cam-3"))
 	// go executeCmd("cd ../client; yarn start")
 	executeCmd(fmt.Sprintf("%s/mediamtx", path))
 }
