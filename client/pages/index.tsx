637b5c20c05f5b510b7c121ca8d767a9ebdf77e1
Trevor Vaughn Hauck <hauck.trevor@gmail.com>
2023-06-05 16:05:30 -0400

    WIP changes

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,28 @@
 import { useEffect, useState } from "react";
 import WHEPClient from "../utils/WHEPClient";
-
+import axios, { AxiosResponse } from "axios";
+const streamBase = "http://192.168.0.140:8889";
 // const stream1Url = "http://192.168.0.140:8889/cam-3";
 // const stream2Url = "http://192.168.0.140:8889/cam-2";
+
+const getOptions = async (url: string) => {
+    const res = await axios({
+        url,
+        method: "OPTIONS",
+    });
+    // console.log(res);
+    return res;
+};
+const createPeerConnection = async (options: any) => {
+    if (options && options.headers) {
+        console.log(options.headers.get("Link"));
+    }
+};
+const createVideoElement = async (url: string) => {
+    const options = await getOptions(url);
+
+    const peerConn = await createPeerConnection(options);
+};
 
 export default function Home() {
     const [show, setShow] = useState(false);
@@ -10,8 +30,11 @@ export default function Home() {
     useEffect(() => {
         setTimeout(() => {
             new WHEPClient("1");
-            // new WHEPClient("2");
-            // new WHEPClient("3");
+            new WHEPClient("2");
+            new WHEPClient("3");
+            const url = `${streamBase}/cam-2/whep`;
+
+            createVideoElement(url);
             setShow(true);
         }, 200);
     });
