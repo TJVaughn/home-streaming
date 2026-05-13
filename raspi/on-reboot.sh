153e911f35021d6f4ea0b7c8602a44cc1be7e565
vaughn <hauck.trevor@gmail.com>
2026-05-13 09:43:22 -0400

    update raspi to use latest rpicam-video binary for headless 32 bit ras pi os- debian trixie

--- raspi/on-reboot.sh
+++ raspi/on-reboot.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-/usr/bin/raspivid -n -ih -t 0 -rot 270 -w 1280 -h 720 -fps 30 -b 1000000 -o - | /usr/bin/ncat -lkv4 2222
+while true; do
+    /usr/bin/rpicam-vid --nopreview --inline --timeout 0 --codec h264 \
+        --width 1280 --height 720 --framerate 30 --bitrate 1000000 \
+        -o - | /usr/bin/ncat -lkv4 2222
+    echo "Stream exited, restarting in 2s..."
+    sleep 2
+done
